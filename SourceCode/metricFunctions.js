fb4eb883e06e59af82ea341b3c3a95f921b71dc5
Divij Kanwar <z5378269@ad.unsw.edu.au>
2024-12-06 19:24:37 +0530

    Gross Profit Margin metric added in function

--- SourceCode/metricFunctions.js
+++ SourceCode/metricFunctions.js
@@ -9,22 +9,33 @@ function readFileDir(fileDir) {
 
 function metricsCalculations(fileData) {
     let revenue = 0;
-    let expense = 0;
+    let expenses = 0;
+    let grossProfit = 0;
+    let grossProfitMargin = 0;
 
     for (let i = 0; i < fileData.data.length; i++) {
         const rowData = fileData.data[i];
         
+        // Revenue Calculation
         if (rowData.account_category === "revenue") {
             revenue += rowData.total_value;
         }
         
+        // Expenses Calculation
         if (rowData.account_category === "expense") {
-            expense += rowData.total_value;
+            expenses += rowData.total_value;
+        }
+
+        // Gross Profit Margin Calculation
+        if (rowData.account_type === "sales" && rowData.value_type === "debit") {
+            grossProfit += rowData.total_value;
         }
     } 
+    grossProfitMargin = revenue !==0 ? grossProfit/revenue : 0;
 
     return {"Revenue": revenue,
-            "Expense": expense
+            "Expense": expenses,
+            "GrossProfitMargin": grossProfitMargin,
     };
 };
 
